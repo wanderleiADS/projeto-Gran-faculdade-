0b42123b00ec54c6089759a6046e330d5d1e9190
wanderlei jose <wanderlei1013@gmail.com>
2024-12-24 14:20:29 -0300

    charset=utf8

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-       res.writehead(200,{'Content-Type':'text/plain'});
-       res.end('Olá, Mundo!')
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end('Olá, Mundo!');
 });
 
-const PORT = 3000
-server.listen(PORT,() => {
+server.listen(3000, 'localhost', () => {
     console.log('Servidor rodando em http://localhost:${3000}/');
 });
