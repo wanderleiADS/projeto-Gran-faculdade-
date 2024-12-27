e28efcd7a83c0b9913ac7a17373335e29c7a5525
wanderlei jose <wanderlei1013@gmail.com>
2024-12-26 22:22:55 -0300

    insomnia.js

---
+++ Insomnia.js
@@ -0,0 +1,70 @@
+{
+    "_type": "export",
+    "__export_format": 4,
+    "__export_date": "2024-12-26T00:12:45.766Z",
+    "__export_source": "insomnia.desktop.app:v10.3.0",
+    "resources": [
+        {
+            "_id": "req_2df3fbed44a14d549be5fa5158d387e5",
+            "parentId": "wrk_fd8287e259bc42c08eb8898b1b39c94f",
+            "modified": 1735171134186,
+            "created": 1735134961921,
+            "url": "localhost:3000",
+            "name": "Return app.js",
+            "description": "",
+            "method": "GET",
+            "body": {},
+            "parameters": [],
+            "headers": [
+                {
+                    "name": "User-Agent",
+                    "value": "insomnia/10.3.0"
+                }
+            ],
+            "authentication": {},
+            "metaSortKey": -1735134961921,
+            "isPrivate": false,
+            "pathParameters": [],
+            "settingStoreCookies": true,
+            "settingSendCookies": true,
+            "settingDisableRenderRequestBody": false,
+            "settingEncodeUrl": true,
+            "settingRebuildPath": true,
+            "settingFollowRedirects": "global",
+            "_type": "request"
+        },
+        {
+            "_id": "wrk_fd8287e259bc42c08eb8898b1b39c94f",
+            "parentId": null,
+            "modified": 1735006640885,
+            "created": 1735006640885,
+            "name": "primeiro app.js",
+            "description": "",
+            "scope": "collection",
+            "_type": "workspace"
+        },
+        {
+            "_id": "env_c266210e476230ecdd52f43f18885bde885a82e7",
+            "parentId": "wrk_fd8287e259bc42c08eb8898b1b39c94f",
+            "modified": 1735006640890,
+            "created": 1735006640890,
+            "name": "Base Environment",
+            "data": {},
+            "dataPropertyOrder": null,
+            "color": null,
+            "isPrivate": false,
+            "metaSortKey": 1735006640890,
+            "environmentType": "kv",
+            "_type": "environment"
+        },
+        {
+            "_id": "jar_c266210e476230ecdd52f43f18885bde885a82e7",
+            "parentId": "wrk_fd8287e259bc42c08eb8898b1b39c94f",
+            "modified": 1735006640892,
+            "created": 1735006640892,
+            "name": "Default Jar",
+            "cookies": [],
+            "_type": "cookie_jar"
+        }
+    ]
+}
--- newFile.js
+++
@@ -1,5 +0,0 @@
-const { server, PORT } = require('./app');
-
-server.listen(PORT, (3000), {
-    console, : .log('Servidor rodando em http://localhost:${3000}/')
-});
